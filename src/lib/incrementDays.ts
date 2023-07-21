cda9447039ed0d14556252fae45a769640d812a4
MasloMaslane <matimasiarz@gmail.com>
2023-07-21 13:54:14 +0200

    Fix (maybe) crash message uptime

--- src/lib/incrementDays.ts
+++ src/lib/incrementDays.ts
@@ -3,28 +3,11 @@ import cron from "cron";
 import { IInfo } from "./types";
 
 export default function() {
-	let cronTime = JSON.parse(fs.readFileSync("./data/info.json", "utf8")).uptimeCron;
 	if (!fs.existsSync("./data/uptime") || fs.existsSync("./data/crashed")) {
-		fs.writeFileSync("./data/uptime", "0");
+		fs.writeFileSync("./data/uptime", (Math.floor(Date.now() / 1000)).toString());
 
 		if (fs.existsSync("./data/crashed")) {
-			const date = new Date(Date.now());
-			cronTime = `${date.getMinutes()} ${date.getHours()} * * *`;
 			fs.rmSync("./data/crashed");
-
-			const info: IInfo = JSON.parse(fs.readFileSync("./data/info.json", "utf8"));
-			info.uptimeCron = cronTime;
-			fs.writeFileSync("./data/info.json", JSON.stringify(info));
 		}
 	}
-
-	new cron.CronJob(
-		cronTime,
-		async function() {
-			fs.writeFileSync("./data/uptime", (parseInt(fs.readFileSync("./data/uptime", "utf-8")) + 1).toString());
-		},
-		null,
-		true,
-		"Europe/Warsaw"
-	).start();
 }
--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+. /home/pi/.nvm/nvm.sh
 rm -dr dist/
-tsc
-if [ ! $? -eq 0 ] 
+/home/pi/.nvm/versions/node/v20.2.0/bin/tsc
+if [ ! $? -eq 0 ]
 then
 	echo "Typescript didn't compile :("
 	exit
@@ -11,11 +12,17 @@ echo "Typescript compiled~ uwu"
 
 while true
 do
-	node dist
+	/home/pi/.nvm/versions/node/v16.9.1/bin/node dist
 	touch data/crashed
+
+	startTimestamp=`cat ./data/uptime`
+	endTimestamp=`date +%s`
+	uptime=`expr $endTimestamp - $startTimestamp`
+	days=`expr $uptime / 86400`
+
 	curl -X POST `cat webhook-link` -H "Content-Type: application/json" --data-binary @- <<DATA
 	{
-	"content": "<@416162471368327178> (<@257119850026106880>) (@everyone)\n\nETEN HAS WORKED\n`cat ./data/uptime`\nDAYS WITHOUT AN ACCIDENT"
+	"content": "<@416162471368327178> (<@257119850026106880>) (@everyone)\n\nETEN HAS WORKED\n$days\nDAYS WITHOUT AN ACCIDENT"
 	}
 DATA
 done
